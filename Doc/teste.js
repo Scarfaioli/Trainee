cc09b5a01441c2b3d4c2b29233566e97b2541e29
Samuel Faioli <scarf1999@gmail.com>
2022-03-22 16:37:10 -0300

    Bobeira

--- Doc/teste.js
+++ Doc/teste.js
@@ -1,15 +1,1 @@
-
-var s = "Hello World";
-let peixe = 0;
-const teste = () => {
-  const string = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  for (let numero of string) {
-    peixe += numero;
-  }
-  return peixe;
-}
-
-
-console.log(teste());
-console.log(s);
-
+console.log("Hello World")
