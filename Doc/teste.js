889e4a9b9105b1386dc5a84e8a6d15f8a9379490
Luan Diniz <luanminasgerais@hotmail.com>
2022-03-22 15:03:13 -0300

    formated

--- Doc/teste.js
+++ Doc/teste.js
@@ -1,12 +1,12 @@
 
 var s = "Hello World";
-let peixe=0;
+let peixe = 0;
 const teste = () => {
-const string = [1,2,3,4,5,6,7,8,9];
-for(let numero of string){
-peixe += numero;
-}
-return peixe;   
+  const string = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (let numero of string) {
+    peixe += numero;
+  }
+  return peixe;
 }
 
 
