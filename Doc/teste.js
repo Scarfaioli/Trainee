83edb9de15503cddb54a120e0118d87729f680e2
Luan Diniz <luanminasgerais@hotmail.com>
2022-03-22 14:38:40 -0300

    peixao

--- Doc/teste.js
+++ Doc/teste.js
@@ -1,4 +1,12 @@
 var s = "Hello World";
+let peixe=0;
+const teste = () => {
+const string = [1,2,3,4,5,6,7,8,9];
+for(let numero of string){
+peixe += numero;
+}
+}
 
+console.log(peixe);
 console.log(s);
 
