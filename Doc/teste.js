427829ee9ef9fa966cd4d0d75f4f038e41eaecc7
Luan Diniz <luanminasgerais@hotmail.com>
2022-03-22 19:19:20 -0300

    multiplicar

--- Doc/teste.js
+++ Doc/teste.js
@@ -1,15 +1,25 @@
 var s = "Hello World";
 let resultadoFinal = 0;
+let resultadoMultiplicado = 0;
+const string = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
 const teste = () => {
-  const string = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   for (let numero of string) {
     resultadoFinal += numero;
   }
   return resultadoFinal;
 }
 
-console.log('a');
-console.log(teste());
-console.log(s);
+const multiplicar = () => {
+  for (let numeros of string) {
+    resultadoMultiplicado *= numeros;
+  }
+}
 
-console.log("Hello Wolrd");
+
+console.log(teste());
+
+console.log("-------------------------------------");
+
+console.log(multiplicar());
