4cf725cec800beeb3468f8f7566e44733eeb6211
Hugo Pina <hugopina.2000@gmail.com>
2022-03-23 17:30:55 -0300

    Testando git reset

--- Doc/teste.js
+++ Doc/teste.js
@@ -1,6 +1,7 @@
 var s = "Hello World";
 let resultadoFinal = 0;
 let resultadoMultiplicado = 0;
+let resultadoSubtraido = 0;
 const string = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
@@ -15,6 +16,14 @@ const multiplicar = () => {
   for (let numeros of string) {
     resultadoMultiplicado *= numeros;
   }
+  return resultadoMultiplicado;
+}
+
+const subtrair = () => {
+  for(let num of string){
+    resultadoSubtraido -= num;
+  }
+  return resultadoSubtraido;
 }
 
 
@@ -23,3 +32,7 @@ console.log(teste());
 console.log("-------------------------------------");
 
 console.log(multiplicar());
+
+console.log("-------------------------------------");
+
+console.log(subtrair());
