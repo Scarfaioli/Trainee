f9ada6c03afa651b54d2a9df7253006047b2815d
Samuel Faioli <scarf1999@gmail.com>
2022-03-24 18:50:13 -0300

    Teste

---
+++ teste.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333);
+
+app.get("/", (request, response) => {
+  return response.json({
+    helloWord: "Hello Word",
+    App: "NodeJs App"
+  });
+});
